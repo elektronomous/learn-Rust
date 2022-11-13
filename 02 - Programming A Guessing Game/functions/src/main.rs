01b4f66df67f9d1fb77d59b0d84c1fe857f50322
elektronomous <akbarfaza222@gmail.com>
2022-11-14 06:45:54 +0800

    function

--- 02 - Programming A Guessing Game/functions/src/main.rs
+++ 02 - Programming A Guessing Game/functions/src/main.rs
@@ -1,8 +1,15 @@
 fn main() {
+    let (x, y, z) = (1, 2, 3);
+    println!("{} {} {}", x, y, z);
     println!("Hello, world!");
     another_function();
+    accept_arguments(15);
 }
 
 fn another_function() {
     println!("another function");
+}
+
+fn accept_arguments(x: i32) {
+    println!("the argument of x: {}", x);
 }
