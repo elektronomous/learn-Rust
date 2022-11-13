da86a53ea808efdf456981408763bc5fb47fa1cd
elektronomous <akbarfaza222@gmail.com>
2022-11-14 02:59:34 +0800

    compiling using cargo

---
+++ 01 - Getting Started/hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a.d
@@ -0,0 +1,5 @@
+/home/elektronomous/Projects/learn-Rust/01 - Getting Started/hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a: src/main.rs
+
+/home/elektronomous/Projects/learn-Rust/01 - Getting Started/hello_cargo/target/debug/deps/hello_cargo-84cd33c7f338469a.d: src/main.rs
+
+src/main.rs:
---
+++ 01 - Getting Started/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/elektronomous/Projects/learn-Rust/01\ -\ Getting\ Started/hello_cargo/target/debug/hello_cargo: /home/elektronomous/Projects/learn-Rust/01\ -\ Getting\ Started/hello_cargo/src/main.rs
